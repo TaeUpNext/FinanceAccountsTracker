39abedbd5dad6bac6c582f5c07b2cd17b900c40a
Taeupnext <armontaeh17@gmail.com>
2022-01-21 23:16:28 -0500

    added taks

---
+++ main.py
@@ -0,0 +1,15 @@
+#Program using SQL Lite to keep track of "all credit card and bank account totals to see if in the Red or Green"
+
+#Todo
+#* Change total to red if < than 0 and Green if total is > 0
+#*table name for account name, primary key possibly for the account (primary key can be account number, and total amount in)
+
+#* take all account totals and add them together to and display them
+
+#* make GUI
+
+#* ability to show each account totals
+#* ability to edit each account totals 
+#* ability to delete an account 
+
+import sqlite3
